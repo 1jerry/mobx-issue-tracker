902701949fd2b573d81324dd9917ce9752d3ac91
Jerry McRae <git-73ry@sneakemail.com>
2021-10-01 16:44:12 -0700

    Add Issues page

---
+++ src/Issues.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>Nothing to see here</div>;
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Issues from "./Issues";
+import reportWebVitals from "./reportWebVitals";
 
+const Index = () => {
+  const todoApp = () => {
+    window.location.pathname = "/todo";
+  };
+  const issueTracker = () => {
+    window.location.pathname = "/issues";
+  };
+  const page = window.location.pathname;
+  if (page === "/todo") return <App />;
+  if (page === "/issues") return <Issues />;
+  return (
+    <>
+      <button onClick={todoApp}>To do app</button>
+      <button onClick={issueTracker}>Issue tracker</button>
+    </>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
